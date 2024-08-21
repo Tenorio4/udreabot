aeb4bf2c7c352f52c8564628da469d7a2c29f652
Tenorio4 <43972708+Tenorio4@users.noreply.github.com>
2024-08-21 13:00:03 +0200

    Update index.js
    Para hacer ping

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+const express = require('express');
 const { Telegraf } = require('telegraf');
+const app = express();
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// Ruta de ping
+app.get('/ping', (req, res) => {
+  res.send('Bot is running');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running');
+});
 
 // Comando /start
 bot.start((ctx) => ctx.reply('Hola, soy tu bot de Telegram!'));
