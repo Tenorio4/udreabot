880eb96dfa51acf50c0b8f651ec9762e41a3302f
Tenorio4 <43972708+Tenorio4@users.noreply.github.com>
2024-08-27 13:21:17 +0200

    Update index.js
    ups

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { Telegraf } = require('telegraf');
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
 
 const app = express();
 const bot = new Telegraf(process.env.BOT_TOKEN);
