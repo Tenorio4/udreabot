1afc922d42aa5f49c14e24bd2a0b4ede4f3240ae
Tenorio4 <43972708+Tenorio4@users.noreply.github.com>
2024-08-30 13:30:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -198,7 +198,8 @@ async function sumarPuntosAGanador(ganadorUsername) {
 }
 
 // Programación de tareas automáticas
-schedule.scheduleJob('59 11 * * *', async () => { // 23:59 cada día   
+schedule.scheduleJob('schedule.scheduleJob(', async () => { // 23:59 cada día   
+  console.log('Ejecutando tarea diaria...');
   bot.telegram.sendMessage('Ha llegado la hora');
   const today = obtenerFechaHoy();
   try {
