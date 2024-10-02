5efb35cd928419405f583ad994f1ee012ba134dd
Tenorio4 <sergiotenorioinfante@gmail.com>
2024-10-02 19:31:44 +0200

    fondo fix7

--- public/script.js
+++ public/script.js
@@ -16,6 +16,8 @@ var canvas = document.createElement("canvas");
 canvas.style.position = "absolute"; // Asegúrate de que el canvas esté posicionado absolutamente
 canvas.style.top = "0"; // Alineado al top
 canvas.style.left = "0"; // Alineado a la izquierda
+canvas.width = window.innerWidth; // Establecer el ancho del canvas
+canvas.height = window.innerHeight; // Establecer la altura del canvas
 canvas.style.width = "100%"; // Ocupa el 100% del ancho de la ventana
 canvas.style.height = "100%"; // Ocupa el 100% de la altura de la ventana
 document.body.appendChild(canvas);
